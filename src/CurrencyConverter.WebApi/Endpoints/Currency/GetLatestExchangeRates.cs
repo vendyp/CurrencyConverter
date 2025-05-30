94c71a6460e764e8258fb1a9d3542fc7dce9ee3d
vendyp <dwivendypratama@gmail.com>
2025-05-30 21:58:14 +0700

    fix: add validation in GetLatestExchangeRates API

--- src/CurrencyConverter.WebApi/Endpoints/Currency/GetLatestExchangeRates.cs
+++ src/CurrencyConverter.WebApi/Endpoints/Currency/GetLatestExchangeRates.cs
@@ -2,6 +2,7 @@
 using CurrencyConverter.Application.Abstractions;
 using CurrencyConverter.Application.Common;
 using CurrencyConverter.Domain.Enums;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 
@@ -10,7 +11,7 @@ namespace CurrencyConverter.WebApi.Endpoints.Currency;
 [Route("api/currency/rates")]
 public class GetLatestExchangeRates : EndpointBaseAsync
     .WithRequest<GetLatestExchangeRatesRequest>
-    .WithResult<GetLatestExchangeRatesResponse>
+    .WithActionResult<GetLatestExchangeRatesResponse>
 {
     private readonly IDistributedCache _distributedCache;
     private readonly ICurrencyConverterProvider _converterProvider;
@@ -23,15 +24,22 @@ public class GetLatestExchangeRates : EndpointBaseAsync
         _converterProvider = converterProvider;
     }
 
-    public static string RelativePath => "/api/currency/rates";
+    public static string RelativePath => "/api/currency/rates/{0}";
 
-    [HttpGet]
-    public override async Task<GetLatestExchangeRatesResponse> HandleAsync(
-        GetLatestExchangeRatesRequest request,
+    [HttpGet("{Currency}")]
+    public override async Task<ActionResult<GetLatestExchangeRatesResponse>> HandleAsync(
+        [FromRoute] GetLatestExchangeRatesRequest request,
         CancellationToken cancellationToken = new())
     {
+        var validator = new GetLatestExchangeRatesRequestValidation();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return BadRequest();
+        }
+
         var cachedString = await _distributedCache.GetStringAsync(
-            string.Format(Constants.RedisKey.LatestExchangeRatesKeyFormat, request.Currency!.ToString()),
+            string.Format(Constants.RedisKey.LatestExchangeRatesKeyFormat, request.Currency),
             cancellationToken);
         if (!string.IsNullOrEmpty(cachedString))
         {
@@ -41,15 +49,17 @@ public class GetLatestExchangeRates : EndpointBaseAsync
             };
         }
 
+        var currency = CurrencyExtensions.Parse(request.Currency);
+
         var results = await _converterProvider.GetAllCurrencyAsync(new CurrencyConverterRequest
         {
-            Base = request.Currency
+            Base = currency
         }, cancellationToken);
 
         var rates = results.Rates.SelectMany(e => e.Value).ToList();
 
         await _distributedCache.SetStringAsync(
-            string.Format(Constants.RedisKey.LatestExchangeRatesKeyFormat, request.Currency!.ToString()),
+            string.Format(Constants.RedisKey.LatestExchangeRatesKeyFormat, request.Currency),
             DefaultJsonSerializer.Serialize(results.Rates),
             new DistributedCacheEntryOptions
             {
@@ -66,7 +76,25 @@ public class GetLatestExchangeRates : EndpointBaseAsync
 
 public class GetLatestExchangeRatesRequest
 {
-    [FromQuery(Name = "curr")] public Enums.Currency? Currency { get; set; } = Enums.Currency.EUR;
+    [FromRoute(Name = "Currency")] public string Currency { get; set; } = null!;
+}
+
+public class GetLatestExchangeRatesRequestValidation : AbstractValidator<GetLatestExchangeRatesRequest>
+{
+    public GetLatestExchangeRatesRequestValidation()
+    {
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(e => e.Currency)
+            .NotNull()
+            .NotEmpty()
+            .Must(ValidCurrency);
+    }
+
+    private static bool ValidCurrency(string currency)
+    {
+        return CurrencyExtensions.IsDefined(currency);
+    }
 }
 
 public class GetLatestExchangeRatesResponse
--- tests/CurrencyConverter.Tests/Endpoints/Currency/GetLatestExchangeRatesTests.cs
+++ tests/CurrencyConverter.Tests/Endpoints/Currency/GetLatestExchangeRatesTests.cs
@@ -3,7 +3,6 @@ using CurrencyConverter.Application.Common;
 using CurrencyConverter.Domain.Enums;
 using CurrencyConverter.WebApi.Endpoints;
 using CurrencyConverter.WebApi.Endpoints.Currency;
-using Microsoft.AspNetCore.WebUtilities;
 using Shouldly;
 using Xunit.Abstractions;
 
@@ -23,7 +22,7 @@ public class GetLatestExchangeRatesTests : IClassFixture<DefaultWebApplicationFa
     }
 
     [Fact]
-    public async Task TestApi_Should_Return_Ok()
+    public async Task GetLatestExchangeRatesTests_Given_Invalid_Currency_Should_Return_BadRequest()
     {
         var response = await _client.GetAsync(TestGenerateToken.RelativePath);
         response.StatusCode.ShouldBe(HttpStatusCode.OK);
@@ -35,12 +34,28 @@ public class GetLatestExchangeRatesTests : IClassFixture<DefaultWebApplicationFa
 
         var testGenerateTokenResponse = DefaultJsonSerializer.Deserialize<TestGenerateTokenResponse>(content);
 
-        var queryParams = new Dictionary<string, string>
-        {
-            { "curr", nameof(Enums.Currency.IDR) }
-        };
-        var relativePath = QueryHelpers.AddQueryString(GetLatestExchangeRates.RelativePath, queryParams!);
-        var httpRequest = new HttpRequestMessage(HttpMethod.Get, relativePath);
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get,
+            string.Format(GetLatestExchangeRates.RelativePath, Guid.NewGuid().ToString()));
+        httpRequest.Headers.Add("Authorization", $"Bearer {testGenerateTokenResponse!.Token}");
+        response = await _client.SendAsync(httpRequest);
+        response.IsSuccessStatusCode.ShouldBeFalse();
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task GetLatestExchangeRatesTests_Given_Correct_Request_Should_Return_Ok()
+    {
+        var response = await _client.GetAsync(TestGenerateToken.RelativePath);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        var content = await response.Content.ReadAsStringAsync();
+
+#if DEBUG
+        _output.WriteLine($"TestGenerateToken content: {content}");
+#endif
+
+        var testGenerateTokenResponse = DefaultJsonSerializer.Deserialize<TestGenerateTokenResponse>(content);
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get,
+            string.Format(GetLatestExchangeRates.RelativePath, nameof(Enums.Currency.IDR)));
         httpRequest.Headers.Add("Authorization", $"Bearer {testGenerateTokenResponse!.Token}");
         response = await _client.SendAsync(httpRequest);
         content = await response.Content.ReadAsStringAsync();
@@ -56,13 +71,13 @@ public class GetLatestExchangeRatesTests : IClassFixture<DefaultWebApplicationFa
 #if DEBUG
         _output.WriteLine($"Key content: {key}");
 #endif
-        
+
         var url = string.Format(TestRedis.RelativePath, key);
 
 #if DEBUG
         _output.WriteLine($"Url content: {url}");
 #endif
-        
+
         response = await _client.GetAsync(string.Format(TestRedis.RelativePath, key));
         content = await response.Content.ReadAsStringAsync();
         response.IsSuccessStatusCode.ShouldBeTrue();
