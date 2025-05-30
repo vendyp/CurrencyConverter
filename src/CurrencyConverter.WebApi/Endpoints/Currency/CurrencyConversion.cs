081f0056b95435e1ed16ab5a2d25ca3708493421
vendyp <dwivendypratama@gmail.com>
2025-05-30 22:16:54 +0700

    fix: CurrencyConversion phase 1, validation

---
+++ src/CurrencyConverter.WebApi/Endpoints/Currency/CurrencyConversion.cs
@@ -0,0 +1,99 @@
+﻿using Ardalis.ApiEndpoints;
+using CurrencyConverter.Application;
+using CurrencyConverter.Domain.Enums;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverter.WebApi.Endpoints.Currency;
+
+[Route("api/currency/conversion")]
+public class CurrencyConversion : EndpointBaseAsync
+    .WithRequest<CurrencyConversionRequest>
+    .WithActionResult<CurrencyConversionResponse>
+{
+    public static string RelativePath => "/api/currency/conversion";
+    
+    private readonly CurrencyConverterManager _currencyConverterManager;
+
+    public CurrencyConversion(CurrencyConverterManager currencyConverterManager)
+    {
+        _currencyConverterManager = currencyConverterManager;
+    }
+
+    [HttpPost]
+    public override async Task<ActionResult<CurrencyConversionResponse>> HandleAsync(
+        [FromBody] CurrencyConversionRequest request,
+        CancellationToken cancellationToken = new())
+    {
+        var validator = new CurrencyConversionRequestValidation(_currencyConverterManager);
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return BadRequest();
+        }
+
+        throw new NotImplementedException();
+    }
+}
+
+public class CurrencyConversionRequest
+{
+    public string? BaseCurrency { get; set; }
+    public decimal? Amount { get; set; }
+    public string? ToCurrency { get; set; }
+}
+
+public class CurrencyConversionRequestValidation : AbstractValidator<CurrencyConversionRequest>
+{
+    public CurrencyConversionRequestValidation(CurrencyConverterManager currencyConverterManager)
+    {
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(e => e.BaseCurrency)
+            .NotNull()
+            .NotEmpty()
+            .Must(CurrencyExtensions.IsDefined!).Custom((curr, context) =>
+            {
+                if (CurrencyExtensions.TryParse(curr, out var value))
+                {
+                    if (currencyConverterManager.ExcludedCurrencies.Any(e => value == e))
+                    {
+                        context.AddFailure("Invalid currency");
+                    }
+                }
+                else
+                {
+                    context.AddFailure("Invalid currency");
+                }
+            });
+
+        RuleFor(e => e.Amount)
+            .NotNull()
+            .GreaterThan(0);
+
+        RuleFor(e => e.ToCurrency)
+            .NotNull()
+            .NotEmpty()
+            .Must(CurrencyExtensions.IsDefined!).Custom((curr, context) =>
+            {
+                if (CurrencyExtensions.TryParse(curr, out var value))
+                {
+                    if (currencyConverterManager.ExcludedCurrencies.Any(e => value == e))
+                    {
+                        context.AddFailure("Invalid currency");
+                    }
+                }
+                else
+                {
+                    context.AddFailure("Invalid currency");
+                }
+            });
+    }
+}
+
+public class CurrencyConversionResponse
+{
+    public decimal? BaseAmount { get; set; }
+    public decimal? Amount { get; set; }
+    public decimal? ToAmount { get; set; }
+}
---
+++ tests/CurrencyConverter.Tests/Endpoints/Currency/CurrencyConversionTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Text;
+using CurrencyConverter.Application.Common;
+using CurrencyConverter.Domain.Enums;
+using CurrencyConverter.WebApi.Endpoints;
+using CurrencyConverter.WebApi.Endpoints.Currency;
+using Shouldly;
+using Xunit.Abstractions;
+
+namespace CurrencyConverter.Tests.Endpoints.Currency;
+
+public class CurrencyConversionTests : IClassFixture<DefaultWebApplicationFactory>
+{
+    private readonly ITestOutputHelper _output;
+    private readonly HttpClient _client;
+
+    public CurrencyConversionTests(
+        DefaultWebApplicationFactory factory,
+        ITestOutputHelper output)
+    {
+        _output = output;
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task CurrencyConversion_Given_Invalid_Request_Should_Return_BadRequest()
+    {
+        var response = await _client.GetAsync(TestGenerateToken.RelativePath);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        var content = await response.Content.ReadAsStringAsync();
+
+#if DEBUG
+        _output.WriteLine($"TestGenerateToken content: {content}");
+#endif
+
+        var testGenerateTokenResponse = DefaultJsonSerializer.Deserialize<TestGenerateTokenResponse>(content);
+
+
+        var request = new CurrencyConversionRequest
+        {
+            BaseCurrency = nameof(Enums.Currency.IDR),
+            Amount = 10000m,
+            ToCurrency = nameof(Enums.Currency.PLN) // excluded currency
+        };
+
+#if DEBUG
+        _output.WriteLine($"TestGenerateToken content: {content}");
+#endif
+
+        var httpRequest = new HttpRequestMessage(HttpMethod.Post, CurrencyConversion.RelativePath);
+        httpRequest.Headers.Add("Authorization", $"Bearer {testGenerateTokenResponse!.Token}");
+        httpRequest.Content = new StringContent(
+            DefaultJsonSerializer.Serialize(request),
+            Encoding.UTF8,
+            "application/json");
+        response = await _client.SendAsync(httpRequest);
+        response.IsSuccessStatusCode.ShouldBeFalse();
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+}
