1da57833b572449632e2d11374185c86616de0dd
vendyp <dwivendypratama@gmail.com>
2025-05-30 23:06:06 +0700

    fix: GetExchangeRates validation

---
+++ src/CurrencyConverter.WebApi/Endpoints/Currency/GetExchangeRates.cs
@@ -0,0 +1,104 @@
+﻿using Ardalis.ApiEndpoints;
+using CurrencyConverter.Application;
+using CurrencyConverter.Domain.Enums;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverter.WebApi.Endpoints.Currency;
+
+[Route("api/currency/rates")]
+public class GetExchangeRates : EndpointBaseAsync
+    .WithRequest<GetExchangeRatesRequest>
+    .WithActionResult<GetExchangeRatesResponse>
+{
+    private readonly CurrencyConverterManager _converterManager;
+
+    public GetExchangeRates(CurrencyConverterManager converterManager)
+    {
+        _converterManager = converterManager;
+    }
+
+    public static string RelativePath => "/api/currency/rates";
+
+    [HttpGet]
+    public override async Task<ActionResult<GetExchangeRatesResponse>> HandleAsync(
+        [FromQuery] GetExchangeRatesRequest request,
+        CancellationToken cancellationToken = new())
+    {
+        var validator = new GetExchangeRatesRequestValidation(_converterManager);
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return BadRequest();
+        }
+
+        throw new NotImplementedException();
+    }
+}
+
+public class GetExchangeRatesRequest
+{
+    [FromQuery(Name = "from")] public DateTime? From { get; set; }
+    [FromQuery(Name = "to")] public DateTime? To { get; set; }
+    [FromQuery(Name = "base")] public string? BaseCurrency { get; set; }
+}
+
+public class GetExchangeRatesRequestValidation : AbstractValidator<GetExchangeRatesRequest>
+{
+    public GetExchangeRatesRequestValidation(CurrencyConverterManager converterManager)
+    {
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(e => e.BaseCurrency)
+            .NotNull()
+            .NotEmpty()
+            .Must(CurrencyExtensions.IsDefined!)
+            .Custom((curr, context) =>
+            {
+                if (CurrencyExtensions.TryParse(curr, out var value))
+                {
+                    if (converterManager.ExcludedCurrencies.Any(e => value == e))
+                    {
+                        context.AddFailure("Invalid currency");
+                    }
+                }
+                else
+                {
+                    context.AddFailure("Invalid currency");
+                }
+            });
+
+        When(e => e.From is not null, () =>
+        {
+            RuleFor(e => e.From)
+                .Custom((e, context) =>
+                {
+                    var ts = DateTime.UtcNow - e!.Value;
+                    if (ts.TotalDays > 30)
+                    {
+                        context.AddFailure("Maximum date range should be between 30 days");
+                    }
+                });
+
+            When(e => e.To is not null, () =>
+            {
+                RuleFor(e => e.From)
+                    .LessThan(e => e.To);
+
+                RuleFor(e => e)
+                    .Custom((e, context) =>
+                    {
+                        var ts = e.To!.Value - e.From!.Value;
+                        if (ts.TotalDays > 30)
+                        {
+                            context.AddFailure("Maximum date range should be between 30 days");
+                        }
+                    });
+            });
+        });
+    }
+}
+
+public class GetExchangeRatesResponse
+{
+}
---
+++ tests/CurrencyConverter.Tests/Endpoints/Currency/GetExchangeRatesTests.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using CurrencyConverter.Application.Common;
+using CurrencyConverter.Domain.Enums;
+using CurrencyConverter.WebApi.Endpoints;
+using CurrencyConverter.WebApi.Endpoints.Currency;
+using Microsoft.AspNetCore.WebUtilities;
+using Shouldly;
+using Xunit.Abstractions;
+
+namespace CurrencyConverter.Tests.Endpoints.Currency;
+
+public class GetExchangeRatesTests : IClassFixture<DefaultWebApplicationFactory>
+{
+    private readonly ITestOutputHelper _output;
+    private readonly HttpClient _client;
+
+    public GetExchangeRatesTests(
+        DefaultWebApplicationFactory factory,
+        ITestOutputHelper output)
+    {
+        _output = output;
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetExchangeRates_Given_Invalid_Request_Should_Return_BadRequest()
+    {
+        var response = await _client.GetAsync(TestGenerateToken.RelativePath);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        var content = await response.Content.ReadAsStringAsync();
+
+#if DEBUG
+        _output.WriteLine($"TestGenerateToken content: {content}");
+#endif
+
+        var testGenerateTokenResponse = DefaultJsonSerializer.Deserialize<TestGenerateTokenResponse>(content);
+
+        var queryParams = new Dictionary<string, string>
+        {
+            { "base", nameof(Enums.Currency.PLN) } // excluded currency
+        };
+        var relativePath = QueryHelpers.AddQueryString(GetExchangeRates.RelativePath, queryParams!);
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, relativePath);
+        httpRequest.Headers.Add("Authorization", $"Bearer {testGenerateTokenResponse!.Token}");
+        response = await _client.SendAsync(httpRequest);
+        response.IsSuccessStatusCode.ShouldBeFalse();
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task GetExchangeRates_Given_Invalid_Request_Exceed_30_TotalDays_Should_Return_BadRequest()
+    {
+        var response = await _client.GetAsync(TestGenerateToken.RelativePath);
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        var content = await response.Content.ReadAsStringAsync();
+
+#if DEBUG
+        _output.WriteLine($"TestGenerateToken content: {content}");
+#endif
+
+        var testGenerateTokenResponse = DefaultJsonSerializer.Deserialize<TestGenerateTokenResponse>(content);
+
+        var queryParams = new Dictionary<string, string>
+        {
+            { "base", nameof(Enums.Currency.IDR) },
+            { "from", DateTime.UtcNow.AddDays(-33).ToString("yyyy-MM-dd") } //exceed maximum 30 days
+        };
+        var relativePath = QueryHelpers.AddQueryString(GetExchangeRates.RelativePath, queryParams!);
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, relativePath);
+        httpRequest.Headers.Add("Authorization", $"Bearer {testGenerateTokenResponse!.Token}");
+        response = await _client.SendAsync(httpRequest);
+        response.IsSuccessStatusCode.ShouldBeFalse();
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+}
